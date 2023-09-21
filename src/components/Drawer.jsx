2138fe3ab2065446a66eda7dd8f9f72f37fc4f23
Samuel Andrade <samuelandraderive@gmail.com>
2023-09-21 17:09:35 -0500

    registro, productos y navbar

---
+++ src/components/Drawer.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListItem
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+import { useNavigate } from "react-router-dom";
+
+const DrawerComp = () => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const navigate = useNavigate();
+
+  const handleClick = () =>{
+    navigate("sign-up")
+    setOpenDrawer(!openDrawer)
+  }
+
+  return (
+    <>
+      <Drawer
+        anchor="left"
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+        PaperProps={{ style: { width: '60%' } }}
+      >
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton onClick={handleClick} >
+              <ListItemIcon>
+                <AccountCircleIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Registrarse" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Drawer>
+      <IconButton
+        sx={{ color: "white", marginLeft: "auto" }}
+        onClick={() => setOpenDrawer(!openDrawer)}
+      >
+        <MenuIcon color="white" />
+      </IconButton>
+    </>
+  );
+};
+
+export default DrawerComp;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,65 +1,81 @@
 import {
   AppBar,
   Container,
-  Box,
   Stack,
   Typography,
   Toolbar,
   Button,
   IconButton,
   Badge,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import useScrollPosition from "../hooks/useScrollPosition";
 import { Link } from "react-router-dom";
 import { useCart } from "../Context/CartContext";
 import { useNavigate } from "react-router-dom";
+import DrawerComp from "./Drawer";
 
 function Navbar() {
   const scrollPosition = useScrollPosition();
   const { toggleCart, cart } = useCart();
   const navigate = useNavigate();
   const count = cart.length;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.up("sm"));
 
   return (
     <>
       <AppBar
         sx={{
           boxShadow: scrollPosition > 10 ? 5 : 0,
-
-          height: 72,
+          
           bgcolor: "secondary.main",
+  
         }}
       >
-        <Container height={"100%"}>
+        <Container>
           <Stack
             direction={"row"}
             alignItems={"center"}
             justifyContent={"space-between"}
-            // flexWrap={"wrap"}
+            sx={{height:72}}
           >
-            <Link to="/">
+            <Link to="/" style={{ height: "100%", padding: "10px"}}>
               <img
                 src={"/logos.png"}
-                style={{ height: 71, objectFit: "contain" }}
+                style={{ height: "100%"}}
               />
             </Link>
-            <Typography
-              variant="h5"
-              to="/"
-              component={Link}
-              sx={{
-                flexGrow: 1,
-                fontWeight: "bold",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              Caramel Bourborn Coffee
-            </Typography>
-            {/* </Link> */}
+            {isMobile ? (
+              <>
+                <Typography
+                  variant="h5"
+                  to="/"
+                  component={Link}
+                  sx={{
+                    flexGrow: 1,
+                    fontWeight: "bold",
+                    color: "inherit",
+                    textDecoration: "none",
+                  }}
+                >
+                  Caramel Bourborn Coffee
+                </Typography>
+                <Button
+                  variant="contained"
+                  sx={{ mx: 2 }}
+                  onClick={() => navigate("sign-up")}
+                >
+                  Registrarse
+                </Button>
+              </>
+            ) : (
+              <DrawerComp />
+            )}
 
-            <Button variant="contained" sx={{mx:2}} onClick={()=> navigate("sign-up")}>Registrarse</Button>
+            {/* </Link> */}
 
             <IconButton
               sx={{ px: 2, color: "background.default" }}
